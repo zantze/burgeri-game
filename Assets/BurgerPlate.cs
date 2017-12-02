db4332e456b5b4c7e1136a50b57c36050812a327
tuomio <tuomiotulee@gmail.com>
2017-12-02 22:50:03 +0200

    Burger should freeze now after finishing it

--- Assets/BurgerPlate.cs
+++ Assets/BurgerPlate.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class BurgerPlate : MonoBehaviour {
 
-  public Collider2D takeout;
+  bool createOnce = true;
+  bool finishBurger = false;
 
+  public Collider2D takeout;
+  public BurgerPlateScanner scanner;
 	// Use this for initialization
 	void Start () {
+
+    createOnce = true;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
-  void OnTriggerEnter2D(Collider2D other) {
+  void OnTriggerStay2D(Collider2D other) {
 
-    if (other == takeout) {
-      Debug.Log("finish");
+
+    if (other == takeout && createOnce && GetComponent<Rigidbody2D>().velocity.magnitude < 1f) {
+
+      GameObject completeBurger = new GameObject("hello");
+
+      foreach (Collider2D collider in scanner.ingredients) {
+        collider.transform.SetParent(completeBurger.transform);
+
+        Destroy(collider.gameObject.GetComponent<Rigidbody2D>());
+      }
+
+      completeBurger.AddComponent<Rigidbody2D>();
+
+      createOnce = false;
     }
   }
+
 
 
 }
