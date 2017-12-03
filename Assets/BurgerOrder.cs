0e3248e7ffbda44ca7e0731607da78dff7beb1cf
tuomio <tuomiotulee@gmail.com>
2017-12-03 15:27:32 +0200

    Started working on delivering the burger

---
+++ Assets/BurgerOrder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurgerOrder : MonoBehaviour {
+
+  public float dirtiness;
+  public Collider2D[] plates;
+
+  Collider2D collider;
+
+	// Use this for initialization
+	void Start () {
+    collider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  private void OnCollisionStay2D(Collision2D collision) {
+
+    bool check = false;
+    foreach (Collider2D thing in plates) {
+      if (collision.collider == thing) {
+        check = true;
+      }
+    }
+
+    if (!check) {
+      dirtiness += Time.deltaTime;
+    }
+  }
+}
--- Assets/burgerSpawn.cs
+++ Assets/burgerSpawn.cs
@@ -4,33 +4,56 @@ using UnityEngine;
 
 public class burgerSpawn : MonoBehaviour {
 
+  public List<Ingredient> recipe;
+  public List<Ingredient> ingredients;
+
   GameObject burger;
   public Material spriteMaterial;
   public GameObject player;
 
+  public Collider2D[] colliderIgnores;
+
   void Awake() {
+    // Find the burger from the previous scene
     burger = GameObject.Find("CreatedBurger");
 
-    burger.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+    // Scale down the burger
+    burger.transform.localScale = new Vector3(0.55f, 0.55f, 0.55f);
 
-    foreach (Transform child in burger.transform) {
-      child.GetComponent<BurgerIngredient>().PlaySounds = false;
-      child.GetComponent<SpriteRenderer>().material = spriteMaterial;
-
-      var lista = new List<Collider2D>() {
+    // Get list with player's collider so we can ignore collision between player and burger part
+    var lista = new List<Collider2D>() {
         player.GetComponent<Collider2D>()
       };
 
+    foreach (Transform child in burger.transform) {
+
+      // Remove sound and add material to burger part
+      child.GetComponent<BurgerIngredient>().PlaySounds = false;
+      child.GetComponent<SpriteRenderer>().material = spriteMaterial;
+
+      // Add IgnoreCollision script to our burger part
       IgnoreCollisions script = child.gameObject.AddComponent<IgnoreCollisions>();
 
+      // Remove player and burger part collision
       script.RemoveCollisions(lista);
+
+      // Add the ingredients into this burgers recipe
+      ingredients.Add(child.GetComponent<BurgerIngredient>().GetIngredient());
     }
 
+    // Change burger gravity
     burger.GetComponent<Rigidbody2D>().gravityScale = 0.4f;
 
+    // Lets create a component for the burger, that carries the data over to 'review' scene
+    BurgerOrder bo = burger.AddComponent<BurgerOrder>();
+    // We need to get some ignores so we can exclude plate and the initial table from dirtiness meter
+    bo.plates = colliderIgnores;
+
+    // Not sure what these are
     IgnoreCollisions ic = burger.AddComponent<IgnoreCollisions>();
     ic.colliders.Add(player.GetComponent<Collider2D>());
 
+    // Move the burger to the spawn location
     burger.transform.position = transform.position;
   }
 
